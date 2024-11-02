cdaab2105a8f7967e2048210f074d40217704d8b
Shakil Khan <dev.shakilkhan@gmail.com>
2024-11-02 22:21:02 +0600

    removed official name and added bg

--- src/Country.jsx
+++ src/Country.jsx
@@ -1,8 +1,8 @@
 const Country = ({ country }) => {
     console.log(country);
     return (
-        <div style={{border: "1px solid purple", borderRadius: "10px", margin:"15px"}}>
-            <h3>{country.name?.official}</h3>
+        <div style={{border: "1px solid purple", borderRadius: "10px", margin:"15px", backgroundColor: "purple"}}>
+            
             <h3>{country.name?.common}</h3>
             <img src={country.flags?.png} alt="" />
         </div>
