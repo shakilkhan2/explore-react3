9799851e81cb85e3363e54ffe11b64fb99897893
Shakil Khan <dev.shakilkhan@gmail.com>
2024-11-04 00:43:30 +0600

    conditional btn added

--- src/Country.jsx
+++ src/Country.jsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+
 const Country = ({ country }) => {
-  const { name, flags, population, area, cca3 } = country;
+    const { name, flags, population, area, cca3 } = country;
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+}
+
   return (
     <div
       style={{
@@ -13,8 +21,15 @@ const Country = ({ country }) => {
       <img style={{ height: "10rem" }} src={flags?.png} alt="" />
       <h4>Shortcode: {cca3}</h4>
       <h4>Area: {area}</h4>
-          <h4> Population: {population}</h4>
-          <button style={{backgroundColor: "purple"}}>Visited</button>
+      <h4> Population: {population}</h4>
+      <button
+        onClick={handleVisited}
+        style={{ backgroundColor: "purple" }}
+        disabled={visited}
+      >
+        {visited ? "Visited" : "Visit"}
+      </button>
+      
     </div>
   );
 };
