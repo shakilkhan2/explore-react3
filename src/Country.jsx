aeb3fa405702dc2306117871d70b5c17b49af16f
Shakil Khan <dev.shakilkhan@gmail.com>
2024-11-03 19:56:35 +0600

    visited button added

--- src/Country.jsx
+++ src/Country.jsx
@@ -1,16 +1,20 @@
 const Country = ({ country }) => {
-  console.log(country);
+  const { name, flags, population, area, cca3 } = country;
   return (
     <div
       style={{
         border: "1px solid purple",
         borderRadius: "10px",
-              margin: "15px",
-        padding: "10px"
+        margin: "15px",
+        padding: "10px",
       }}
     >
-      <h3>{country.name?.common}</h3>
-      <img style={{height: "10rem"}} src={country.flags?.png} alt="" />
+      <h3>{name?.common}</h3>
+      <img style={{ height: "10rem" }} src={flags?.png} alt="" />
+      <h4>Shortcode: {cca3}</h4>
+      <h4>Area: {area}</h4>
+          <h4> Population: {population}</h4>
+          <button style={{backgroundColor: "purple"}}>Visited</button>
     </div>
   );
 };
